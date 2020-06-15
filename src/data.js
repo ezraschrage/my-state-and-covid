a02de31df50808f3f01e8a0b3599ab5464a9f749
ezschrage <ezschrage@gmail.com>
2020-06-15 02:16:37 -0400

    Remove consoles

--- src/data.js
+++ src/data.js
@@ -2,13 +2,8 @@ let states = {};
 // console.log(states)
 d3.csv("https://covidtracking.com/api/v1/states/current.csv", function (data) {
     for (let i = 0; i < data.length; i += 1) {
-        // console.log(data[i].positive);
-        // console.log(data[i].negative);
-        // console.log(data[i])
         let state = data[i];
         let name = state.state;
-        // console.log(name)
-        // console.log(typeof states)
         states[`${name}`] = {
             positive: parseInt(state.positive),
             negative: parseInt(state.negative),
@@ -22,6 +17,7 @@ d3.csv("https://covidtracking.com/api/v1/states/current.csv", function (data) {
             onVentilatorCumulative: parseInt(state.onVentilatorCumulative),
             recovered: parseInt(state.recovered), 
             hospitalized: parseInt(state.hospitalized)
-        }        
+        }
+        
     }
 });
