5343c8b2f00a21ba6b8a242c1225516ebd9afc9f
ezschrage <ezschrage@gmail.com>
2020-06-15 02:37:05 -0400

    State names

--- src/index.js
+++ src/index.js
@@ -19,3 +19,93 @@
 //   document.body.appendChild(imgCard);
 // });
 
+const stateNames = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AS": "American Samoa",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "District Of Columbia",
+    "FM": "Federated States Of Micronesia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "GU": "Guam",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MH": "Marshall Islands",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "MP": "Northern Mariana Islands",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PW": "Palau",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+}
+
+const states = {};
+// console.log(states)
+d3.csv("https://covidtracking.com/api/v1/states/current.csv", function (data) {
+    for (let i = 0; i < data.length; i += 1) {
+        const state = data[i];
+        const name = state.state;
+        states[`${name}`] = {
+            abreviation: name,
+            name: stateNames[name],
+            positive: parseInt(state.positive),
+            negative: parseInt(state.negative),
+            death: parseInt(state.death),
+            totalTestResults: parseInt(state.totalTestResults),
+            hospitalizedCurrently: parseInt(state.hospitalizedCurrently),
+            hospitalizedCumulative: parseInt(state.hospitalizedCumulative),
+            inIcuCurrently: parseInt(state.inIcuCurrently),
+            inIcuCumulative: parseInt(state.inIcuCumulative),
+            onVentilatorCurrently: parseInt(state.onVentilatorCurrently),
+            onVentilatorCumulative: parseInt(state.onVentilatorCumulative),
+            recovered: parseInt(state.recovered),
+            hospitalized: parseInt(state.hospitalized)
+        }
+        // console.log(states)
+    }
+    console.log(states)
+}
+
+);
